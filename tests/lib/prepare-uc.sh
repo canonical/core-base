7f870eeb59b8ede5d97e43d3d75bf0c98b5ee39f
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2026-02-23 20:26:45 -0500

    tests/lib/prepare-uc.sh: do not use host users
    They are very different to what we have in the boot base and this was breaking
    the tests.

--- tests/lib/prepare-uc.sh
+++ tests/lib/prepare-uc.sh
@@ -111,7 +111,8 @@ echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/99-ubuntu-user
 sed -i 's/\#\?\(PermitRootLogin\|PasswordAuthentication\)\>.*/\1 yes/' /etc/ssh/sshd_config
 echo "MaxAuthTries 120" >> /etc/ssh/sshd_config
 grep '^PermitRootLogin yes' /etc/ssh/sshd_config
-systemctl reload ssh
+# sshd is socket activated in UC24+, so the reload might fail
+systemctl reload ssh || true
 print_system "done setting up ssh for spread test user"
 touch /root/spread-setup-done
 EOF
@@ -137,41 +138,34 @@ build_base_image
 # setup some data we will inject into ubuntu-seed partition of the image above
 # that snapd.spread-tests-run-mode-tweaks.service will ingest
 
+# We get the base user data from the boot base
+base_d=base
+unsquashfs -d "$base_d" "$uc_snap"
+
 # this sets up some /etc/passwd and group magic that ensures the test and ubuntu
 # users are working, mostly copied from snapd spread magic
 mkdir -p /root/test-etc
-# NOTE that we don't use the real extrausers db on the host VM here because that
-# could be used to actually login to the L1 VM, which we don't want to allow, so
-# put it in a fake dir that login() doesn't actually look at for the host L1 VM.
 mkdir -p /root/test-var/lib/extrausers
 touch /root/test-var/lib/extrausers/sub{uid,gid}
 for f in group gshadow passwd shadow; do
-    # don't include the ubuntu user here, we manually add that later on
-    grep -v "^root:" /etc/"$f" | grep -v "^ubuntu:" /etc/"$f" > /root/test-etc/"$f"
-    grep "^root:" /etc/"$f" >> /root/test-etc/"$f"
-    chgrp --reference /etc/"$f" /root/test-etc/"$f"
-    # append test user for testing
-    grep "^test:" /etc/"$f" >> /root/test-var/lib/extrausers/"$f"
-    # check test was copied
-    MATCH "^test:" < /root/test-var/lib/extrausers/"$f"
+    # We bind mount this directory when we run
+    cp "$base_d"/etc/"$f" /root/test-etc/
+    # These files are copied when we run
+    cp "$base_d"/var/lib/extrausers/"$f" /root/test-var/lib/extrausers/
 done
+
+rm -rf "$base_d"
 
 # TODO: could we just do this in the script above with adduser --extrausers and
 # echo ubuntu:ubuntu | chpasswd ?
-# dynamically create the ubuntu user in our fake extrausers with password of 
+# dynamically create the ubuntu user in our fake extrausers with password of
 # ubuntu
 #shellcheck disable=SC2016
 echo 'ubuntu:$6$5jPdGxhc$8DgCHDdjj9IQxefS9atknQq4JVVYqy6KiPV/p4fDf5NUI6dqKTAf0vUZNx8FUru/pNgOQMwSMzS5pFj3hp4pw.:18492:0:99999:7:::' >> /root/test-var/lib/extrausers/shadow
-#shellcheck disable=SC2016
-echo 'ubuntu:$6$5jPdGxhc$8DgCHDdjj9IQxefS9atknQq4JVVYqy6KiPV/p4fDf5NUI6dqKTAf0vUZNx8FUru/pNgOQMwSMzS5pFj3hp4pw.:18492:0:99999:7:::' >> /root/test-etc/shadow
 echo 'ubuntu:!::' >> /root/test-var/lib/extrausers/gshadow
 # use gid of 1001 in case sometimes the lxd group sneaks into the extrausers image somehow...
 echo "ubuntu:x:1000:1001:Ubuntu:/home/ubuntu:/bin/bash" >> /root/test-var/lib/extrausers/passwd
-echo "ubuntu:x:1000:1001:Ubuntu:/home/ubuntu:/bin/bash" >> /root/test-etc/passwd
 echo "ubuntu:x:1001:" >> /root/test-var/lib/extrausers/group
-
-# add the test user to the systemd-journal group if it isn't already
-sed -r -i -e 's/^systemd-journal:x:([0-9]+):$/systemd-journal:x:\1:test/' /root/test-etc/group
 
 # tar the runmode tweaks and copy them to the image
 tar -c -z -f run-mode-overlay-data.tar.gz \
