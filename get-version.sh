9bf041d5a0e93bd5a80746bc700d7460705335bd
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2026-04-06 10:58:36 -0400

    get-version.sh: strip branch suffix from tags before parsing
    Add sed command to remove branch and variant suffixes (everything
    after '_') from tags before version extraction. This ensures tags
    like '20260505-1_main+cloud-init' are correctly parsed as
    '20260505-1'.

--- get-version.sh
+++ get-version.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -ex
 
-tag=$(git tag --points-at HEAD | grep -E '^[0-9]{8}(-[0-9]+)?$' |
+# Tags are expected to be <date>-<seq>_<branch>[+<variant>], for instance:
+#   20260505-1_main+cloud-init
+#   20260505_core22
+#   20260505_core22+fips
+# This commands removes the bits after '_' and takes the tag with the oldest
+# date and sequence number.
+tag=$(git tag --points-at HEAD | sed 's/_.*//g' | grep -E '^[0-9]{8}(-[0-9]+)?$' |
           sort --field-separator=- --key=1n,1 --key=2n,2 | tail -n1 || printf "")
 if [ -z "$tag" ]; then
     # Current date is the default if there are no date tags
