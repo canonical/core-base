4cf1562479c4584fc54ce0bffbf8781150d20e84
Meulengracht <philip.meulengracht@canonical.com>
2026-09-09 11:24:53 +0200

    tests: inject ntp configuration through smbios (#466)
    * tests: inject ntp configuration through smbios
    
    * spread: add default NTP
    
    * tests/spread: simplify grep
    
    * tests/lib: no need to save on variables :)

--- tests/lib/nested.sh
+++ tests/lib/nested.sh
@@ -102,6 +102,18 @@ start_nested_core_vm_unit(){
     PARAM_LOG="-D ${WORK_DIR}/qemu.log"
     PARAM_SERIAL="-chardev socket,telnet=on,host=localhost,server=on,port=7777,wait=off,id=char0,logfile=${WORK_DIR}/serial.log,logappend=on -serial chardev:char0"
     PARAM_TPM=""
+    PARAM_SMBIOS=""
+
+    if [ -n "${NTP_SERVER:-}" ]; then
+        local NTP_SOURCES_FILE NTP_SOURCES_BASE64 NTP_SOURCES_FILE_BASE64
+        NTP_SOURCES_FILE=$(mktemp)
+        NTP_SOURCES_BASE64=$(echo "pool ${NTP_SERVER} iburst maxsources 1 nts prefer" | base64 -w0)
+        cat > "$NTP_SOURCES_FILE" <<EOF
+f+~ /etc/chrony/sources.d/10-nested-ntp.sources 0644 - - - ${NTP_SOURCES_BASE64}
+EOF
+        NTP_SOURCES_FILE_BASE64=$(base64 -w0 "$NTP_SOURCES_FILE")
+        PARAM_SMBIOS="-smbios type=11,value=io.systemd.credential.binary:tmpfiles.extra=${NTP_SOURCES_FILE_BASE64}"
+    fi
 
     # TODO: enable ms key booting for i.e. nightly edge jobs ?
     VMF_CODE=""
@@ -180,6 +192,7 @@ start_nested_core_vm_unit(){
                 ${PARAM_BIOS} \
                 ${PARAM_TPM} \
                 ${PARAM_RANDOM} \
+                ${PARAM_SMBIOS} \
                 ${PARAM_IMAGE} \
                 ${PARAM_SERIAL} \
                 ${PARAM_MONITOR}; then
